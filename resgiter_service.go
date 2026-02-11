c4a10ace128c9873f2724e8371296e6f7a802dcb
Maaz Umar <maaz.umar@cowlar.com>
2026-02-11 18:16:19 +0500

    Fix base URL in configuration and implement proxy handler in service registration
    - Updated base_url for external-service-2 in config.json to point to the correct localhost.
    - Added proxyHandler method in resgiter_service.go to handle requests and forward them to the appropriate service endpoint using a reverse proxy.

--- resgiter_service.go
+++ resgiter_service.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
 func (cfg *Config) registerService(mux *http.ServeMux) {
@@ -14,8 +16,20 @@ func (cfg *Config) registerService(mux *http.ServeMux) {
 			fmt.Printf("%s /%s%s \n", endpoint.Method, service.Name, endpoint.Path)
 			mux.HandleFunc(fmt.Sprintf("%s /%s%s", endpoint.Method, service.Name, endpoint.Path), func(w http.ResponseWriter, r *http.Request) {
 				log.Println("request received", r.Method, r.URL.Path)
-				// service.Handler(w, r)
+				service.proxyHandler(w, r, endpoint)
 			})
 		}
 	}
+}
+
+func (service *Service) proxyHandler(w http.ResponseWriter, r *http.Request, endpoint Endpoint) {
+	r.URL.Path = endpoint.Path
+
+	target, err := url.Parse(fmt.Sprintf("%s:%d", service.BaseURL, service.Port)) // e.g. http://BASE_URL:PORT
+	if err != nil {
+		log.Fatalf("failed to parse target: %v", err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(target)
+	proxy.ServeHTTP(w, r)
 }
